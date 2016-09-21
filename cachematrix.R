b37f9b92134d241afcaff59e7fa200cf53dd7e2e
chenwei900402 <chenwei900402@hotmail.com>
2016-09-21 23:18:15 +0800

    Assignment Updated - Chen Wei
    Still little bit confused.....

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Chen Wei
+## Cache the Matrix for Inversion
 
-## Write a short comment describing this function
+## Create Object to cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+       if(as.integer(sqrt(length(x))) != sqrt(length(x))){
+               message('This is not a square Matrix')
+       } else {
+               dim(x) <- c(sqrt(length(x)),sqrt(length(x)))
+       }
+        w <- NULL
+        set <- function(y){
+                x <<- y
+                w <<- NULL
+        }
+        get <- function()x
+        setsolve <- function(solve){ w <<- solve}
+        getsolve <- function() w
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)    
 }
 
 
-## Write a short comment describing this function
+## Do the inversion
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        w <- x$getsolve()
+        if(!is.null(w)){
+                message('getting cached data')
+                return(w)
+        }
+        data <- x$get()
+        w <- solve(data)
+        x$setsolve(w)
+        w        
 }
